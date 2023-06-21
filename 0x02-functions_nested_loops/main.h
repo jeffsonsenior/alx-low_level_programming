a14ddca23c61ab122570c6adac44fece79e9919c
jeffsonsenior <kinyuajeff@gmail.com>
2023-06-21 15:34:37 -0700

    0. _putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,4 @@
+#ifndef main_h
 #define main_h
 int _putchar(char);
 void print_alphabet(void);
@@ -12,4 +13,4 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
-
+#endif
