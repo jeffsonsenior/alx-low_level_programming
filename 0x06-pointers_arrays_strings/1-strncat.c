87052935d099036826823c9163d0208a09e5d752
jeffsonsenior <kinyuajeff@gmail.com>
2023-06-29 18:18:59 -0700

    2. strncpy

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -7,6 +7,7 @@
  * @n:Input value
  *
  * Return: dest
+ *
  */
 char *_strncat(char *dest, char *src, int n)
 {
@@ -26,5 +27,6 @@ char *_strncat(char *dest, char *src, int n)
 	j++;
 	}
 	dest[i] = '\0';
+	
 	return (dest);
 }
