2c6dcba82d6f94299ecf0441bc18abefc53378b8
jeffsonsenior <kinyuajeff@gmail.com>
2023-06-29 19:48:40 -0700

    9. Numbers have life; they're not just symbols on paper

--- 0x06-pointers_arrays_strings/_putchar.c
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -4,8 +4,8 @@
  * _putchar - writes the character c to stdout
  * @a: The character to print
  * a: The character to print
- *Return: On success return 1
- * On error, -1 is returned, and errno is set appropriately
+ *Return: On success return one
+ * On error, -one is returned, and errno is set appropriately
  */
 int _putchar(char c )
 {
