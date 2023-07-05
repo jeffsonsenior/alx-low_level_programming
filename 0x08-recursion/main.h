346e7c40b2cb3e6421e14eea72a8a01f264437e1
jeffsonsenior <kinyuajeff@gmail.com>
2023-07-05 13:01:19 -0700

    She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,4 +1,4 @@
-ifndef MAIN_H
+#ifndef MAIN_H
 #define MAIN_H
 
 void _puts_recursion(char *s);
@@ -11,4 +11,3 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
 #endif
-
