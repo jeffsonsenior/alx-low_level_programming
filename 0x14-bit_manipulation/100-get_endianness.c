96f43aefc66719207656b32f51f2e7a76ba55d92
jeffsonsenior <kinyuajeff@gmail.com>
2023-09-29 12:41:36 -0700

    password

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -3,13 +3,11 @@
 /**
  * get_endianness - checks if endian is little or big.
  *
- *Returns: 0 if big endian, 1 if little endian.
+ * Returns: 0 if big endian, 1 if little endian.
  */
 int get_endianness(void)
 {
 	unsigned int i = 1;
 	char *c = (char *) &i;
-
 	return (*c);
 }
-
