670e151c407cc1553823c1b9cb4e08b7ccd47944
jeffsonsenior <kinyuajeff@gmail.com>
2023-09-29 14:03:41 -0700

    Crackme3

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,13 +1,13 @@
 #include "main.h"
-
 /**
- * get_endianness - checks if endian is little or big.
- *
- * Returns: 0 if big endian, 1 if little endian.
+ * get_endianness - checks if endian is  little or big.
+ * Return: 0 for big, 1 for little.
  */
 int get_endianness(void)
 {
 	unsigned int i = 1;
 	char *c = (char *) &i;
+
 	return (*c);
 }
+
