dfeb7b76f89b053ba6a868e000e7fb37a2397ac3
jeffsonsenior <kinyuajeff@gmail.com>
2023-11-27 17:50:03 -0800

    append text to file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,39 +1,29 @@
 #include "main.h"
-
 /**
- * create_file:  create a file
- * @filename: filename
- * @text_content: writed inside the file.
- *
- * Return: 1 if it success. -1 if error occures
+ * create_file - creates a file and write  text content
+ * @filename: the name of the file to create
+ * @text_content: A NULL terminated string to write to the file
+ * Return: 1 success, -1 failure
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
-	int nletters;
-	int rwr;
+	int file, total = 1, count = 0;
 
-	if (!filename)
+	if (filename == NULL)
+	{
 		return (-1);
-
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-
-	if (fd == -1)
+	}
+	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (file == -1)
 		return (-1);
-
-	if (!text_content)
-		text_content = "";
-
-	for (nletters = 0; text_content[nletters]; nletters++)
-		;
-
-	rwr = write(fd, text_content, nletters);
-
-	if (rwr == -1)
+	if (text_content)
+	{
+		while (text_content[count])
+			count++;
+		total = write(file, text_content, count);
+	}
+	if (total == -1)
 		return (-1);
-
-	close(fd);
-
+	close(file);
 	return (1);
 }
-
--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -9,31 +9,26 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd;
-	int nletters;
-	int rwr;
+	int fd, _write;
 
 	if (!filename)
+	{
 		return (-1);
-
+	}
 	fd = open(filename, O_WRONLY | O_APPEND);
-
 	if (fd == -1)
 		return (-1);
-
-	if (text_content)
+	if (!text_content)
 	{
-		for (nletters = 0; text_content[nletters]; nletters++)
-			;
-
-		rwr = write(fd, text_content, nletters);
-
-		if (rwr == -1)
-			return (-1);
+		close(fd);
+		return (1);
 	}
-
+	_write = write(fd, text_content, strlen(text_content));
+	if (_write == -1)
+	{
+		close(fd);
+		return (-1);
+	}
 	close(fd);
-
 	return (1);
 }
-
